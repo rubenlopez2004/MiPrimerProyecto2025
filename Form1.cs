1fcda1cc2d0cc55169c1a8f9bae1905e6ee1899c
rubenlopez2004 <rglc.ironteam@gmail.com>
2025-08-26 15:36:25 -0600

    primer cambio

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace MiPrimerProyecto2025
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+        }
     }
 }
